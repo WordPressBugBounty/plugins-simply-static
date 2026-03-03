7bc168550fcee5536f08e8a57968278056398be3
Sean Murphy <sean@iamseanmurphy.com>
2026-03-03 12:26:13 +0000

    3.6.2

--- simply-static/src/admin/build/index.asset.php
+++ simply-static/src/admin/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-api-fetch', 'wp-components', 'wp-element'), 'version' => 'bb697653f4b9d7d77376');
+<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-api-fetch', 'wp-components', 'wp-element'), 'version' => 'dfdfa28ec0e3807249dc');
